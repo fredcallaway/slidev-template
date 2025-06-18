89930bd6f0c2a6cf333ecc6f5d4d83acf2591b26
fredcallaway <fredc@llaway.com>
2025-06-18 09:04:07 -0400

    hide nav bar, smaller drauu bar, drauu toggle with d

---
+++ setup/shortcuts.ts
@@ -0,0 +1,32 @@
+import type { NavOperations, ShortcutOptions } from '@slidev/types'
+import { defineShortcutsSetup } from '@slidev/types'
+import { useDrawings } from '@slidev/client'
+
+
+const {
+  brush,
+  drawingEnabled,
+} = useDrawings()
+
+
+export default defineShortcutsSetup((nav: NavOperations, base: ShortcutOptions[]) => {
+  return [
+    ...base, // keep the existing shortcuts
+    {
+      key: 'enter',
+      fn: () => nav.next(),
+      autoRepeat: true,
+    },
+    {
+      key: 'backspace',
+      fn: () => nav.prev(),
+      autoRepeat: true,
+    },
+    {
+      key: 'd',
+      fn: () => {
+        drawingEnabled.value = !drawingEnabled.value
+      }
+    }
+  ]
+})
