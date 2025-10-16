dfb32adb3661a054240ae3253153a4e7df149003
fredcallaway <fredc@llaway.com>
2025-10-16 11:15:31 -0400

    Pointer2

---
+++ theme/components/Pointer2.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  x: string
+  y: string
+  x2: string
+  y2: string
+  width?: string
+}>()
+
+// Calculate arrow coordinates based on props
+const arrowCoords = computed(() => {
+  const x = parseFloat(props.x) * 10
+  const y = parseFloat(props.y) * 10
+  const x2 = parseFloat(props.x2) * 10
+  const y2 = parseFloat(props.y2) * 10
+  const width = parseFloat(props.width || '3')
+  
+  return {
+    x2,
+    y2,
+    x1: x,
+    y1: y,
+    width,
+    color: 'black'
+  }
+})
+
+</script>
+
+<template>
+  <Arrow v-bind="arrowCoords" />
+</template>
